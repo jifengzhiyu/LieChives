f43c7f2d1adabb418570d767932fd28234b35d9e
jifengzhiyu <2716105592@qq.com>
2022-02-03 18:02:37 +0800

    8. 测试数据库的查找 9.cell的数据源方法设置： kitchenPartArr记录cell的cell.textLabel.text     cell.textLabel.text = self.kitchenPartArr[indexPath.row];
        ⚠️"unable to dequeue a cell with identifier kitchenCell - must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
    https://blog.csdn.net/args_/article/details/51910930
        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
    
    10.打算使用    cell.accessoryType = UITableViewCellAccessoryCheckmark;
    来表示对勾

--- LieChives/JFKitchenTableViewController.m
+++ LieChives/JFKitchenTableViewController.m
@@ -8,6 +8,8 @@
 #import "JFKitchenTableViewController.h"
 #import "Kitchen+CoreDataClass.h"
 @interface JFKitchenTableViewController ()
+@property (nonatomic, strong) Kitchen *kitchen;
+@property (nonatomic, copy) NSArray *kitchenPartArr;
 
 @end
 
@@ -22,14 +24,51 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    //通过实体描述描述出实体对象
-  Kitchen *kitchen =  [NSEntityDescription insertNewObjectForEntityForName:@"Kitchen" inManagedObjectContext:[JFCoreDataManager sharedManager].managerContext];
+    [self initKitchenData];
+    
+    [self fetchKitchenData];
+    
+}
 
+- (void)fetchKitchenData{
+    //查询请求
+    NSFetchRequest *fetchrequest = [[NSFetchRequest alloc]init];
+    
+    //设置一些参数
+    //实体
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Kitchen" inManagedObjectContext:[JFCoreDataManager sharedManager].managerContext];
+    //设置实体
+    fetchrequest.entity = entity;
+    
+//    //谓词   有点像SQL语句中的 where
+//    NSPredicate *pre = [NSPredicate predicateWithFormat:@"age = %@",@"26"];
+//    //设置一下
+//    fetchrequest.predicate = pre;
+    
+    //排序
+//    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"age" ascending:YES];
+    //设置排序
+//    fetchrequest.sortDescriptors = @[sort];
+    
+    //执行查询请求
+    NSArray *temp = [[JFCoreDataManager sharedManager].managerContext executeFetchRequest:fetchrequest error:nil];
+    
+    //打印结果集
+    for (Kitchen *kitchen  in temp) {
+        NSLog(@"%d--%d---%d---%d 一共有多少个记录%lu",kitchen.gongZuoTai,kitchen.zaoTai,kitchen.chuGui,kitchen.shuiChi,temp.count);
+    }
+}
+
+- (void)initKitchenData{
+    //通过实体描述描述出实体对象
+  Kitchen *kitchen = [NSEntityDescription insertNewObjectForEntityForName:@"Kitchen" inManagedObjectContext:[JFCoreDataManager sharedManager].managerContext];
     
     //数据存储插入操作  KVC
-//    [person setValue:@"leo" forKey:@"name"];
-    kitchen.gongZuoTai = 0;
-    kitchen.zaoTai = 1;
+    kitchen.gongZuoTai = self.kitchen.gongZuoTai;
+    kitchen.zaoTai = self.kitchen.zaoTai;
+    kitchen.chuGui = self.kitchen.chuGui;
+    kitchen.shuiChi = self.kitchen.shuiChi;
+    
     
     //通过上下文进行提交存储
     [[JFCoreDataManager sharedManager].managerContext save:nil];
@@ -38,24 +77,40 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 4;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
     // Configure the cell...
-    
+    static NSString * identifier = @"kitchenCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = self.kitchenPartArr[indexPath.row];
+    cell.tintColor = [UIColor systemGreenColor];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
     return cell;
 }
-*/
+
+- (NSArray *)kitchenPartArr{
+    if(!_kitchenPartArr){
+        _kitchenPartArr = @[@"橱柜", @"工作台", @"水池", @"灶台"];
+    }
+    return _kitchenPartArr;
+}
+
+// 点击 cell 调用
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = (UITableViewCell *)[tableView cellForRowAtIndexPath:indexPath];
+    NSLog(@"选中 cell----> %@",cell.textLabel.text);
+}
 
 /*
 // Override to support conditional editing of the table view.
--- LieChives/Kitchen+CoreDataProperties.h
+++ LieChives/Kitchen+CoreDataProperties.h
@@ -20,6 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) BOOL shuiChi;
 @property (nonatomic) BOOL zaoTai;
 
+
 @end
 
 NS_ASSUME_NONNULL_END

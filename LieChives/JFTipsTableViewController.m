13d170c6211c5b42e75cdd9cbbc35e58283ccafa
jifengzhiyu <2716105592@qq.com>
2022-02-05 18:30:14 +0800

    4. VC 数据源方法设置包括 组标题 产生问题：不换行

--- LieChives/JFTipsTableViewController.m
+++ LieChives/JFTipsTableViewController.m
@@ -10,7 +10,7 @@
 
 @interface JFTipsTableViewController ()
 ///从Tips.plist加载的模型数组
-@property (nonatomic, strong) NSArray *tips;
+@property (nonatomic, strong) NSArray *tipsArr;
 
 
 @end
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"%@",self.tips);
+    NSLog(@"%@",self.tipsArr);
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -33,18 +33,44 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return self.tipsArr.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    JFTips *tip = self.tipsArr[section];
+    return tip.tips.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * identifier = @"tipsCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifier];
+    }
+    
+    // 1. 获取模型数据
+    // 根据组的索引获取对应的组的模型
+    JFTips *tip = self.tipsArr[indexPath.section];
+    // 根据当前行的索引, 获取对应组的对应行的车
+    NSString *tipText = tip.tips[indexPath.row];
+    
+    cell.textLabel.text = tipText;
+    
+    
+    return cell;
+}
+
+// 设置组标题的数据源方法
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    // 根据组的索引获取对应的组的模型
+    JFTips *tip = self.tipsArr[section];
+    return tip.title;
 }
 
 #pragma mark - 懒加载数据
-- (NSArray *)tips{
-    if(_tips == nil){
+- (NSArray *)tipsArr{
+    if(_tipsArr == nil){
         //找到plist路径
         NSString *path = [[NSBundle mainBundle] pathForResource:@"Tips.plist" ofType:nil];
         //加载plist文件
@@ -57,9 +83,9 @@
             JFTips *model = [JFTips tipsWithDict:dic];
             [arrayModel addObject:model];
         }
-        _tips = arrayModel;
+        _tipsArr = arrayModel;
     }
-    return _tips;
+    return _tipsArr;
 }
 
 @end

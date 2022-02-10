ede51871c3b4fbfef9d36025944e1529491c2bd4
jifengzhiyu <2716105592@qq.com>
2022-02-10 18:00:48 +0800

    评分 https://stackoverflow.com/questions/64026344/skstorereviewcontroller-requestreview-not-working-on-ios-14
    版本更新
    https://juejin.cn/post/7050362995704070158
    
    ⚠️Treating Unicode character as whitespace在我们 copy 一些文本到Xcode 里面的时候会有 Treating Unicode character as whitespace 的警告，此时认真检查你 copy 的文本部分，回车、空格是否有问题。当然一般检查不出来，如果 直接删除回车和空格的部分，自己重新输入,这个也是太繁琐了。

--- LieChives/JFSettingTableViewController.m
+++ LieChives/JFSettingTableViewController.m
@@ -6,7 +6,7 @@
 //
 
 #import "JFSettingTableViewController.h"
-
+#import <StoreKit/StoreKit.h>
 @interface JFSettingTableViewController ()
 
 @end
@@ -28,67 +28,130 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 3;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    if(indexPath.row == 0){
+    static NSString * identifier = @"versionCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if(cell == nil){
+       
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = @"当前版本";
+    cell.detailTextLabel.text = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+        
+        cell.userInteractionEnabled = NO;
     
     return cell;
-}
-*/
+    }else if(indexPath.row == 1){
+        static NSString * identifier = @"settingCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+        if(cell == nil){
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+        }
+        cell.textLabel.text = @"去评分";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+        return cell;
+    }else{
+        static NSString * identifier = @"settingCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+        if(cell == nil){
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        }
+        cell.textLabel.text = @"版本更新";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+        return cell;
+    }
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
+#pragma mark - 点击cell
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.row == 1){
+    
+        //弹出评分
+        if([SKStoreReviewController respondsToSelector:@selector(requestReview)]){
+            [SKStoreReviewController requestReviewInScene:self.view.window.windowScene];
+        }
+
+        
+    }else if (indexPath.row == 2){
+        [self requestAppVersion];
+    }
 }
-*/
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 查看更新
+/// 请求接口
+- (void)requestAppVersion {
+    NSString *url = @"https://itunes.apple.com/cn/lookup?bundleId=com.jifengzhiyu-github.www.LieChives";
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    [manager POST:url parameters:@{} headers:@{} progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            NSArray *results = [responseObject objectForKey:@"results"];
+            NSDictionary *resultDic = results.firstObject;
+            if (resultDic) {
+                [self judgeIfNeedUpdate:resultDic];//判断是否有新的版本
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            NSLog(@"%@",error);
+        }];
 }
-*/
+
+/// 判断是否有新的版本
+- (void)judgeIfNeedUpdate:(NSDictionary *)resultDic {
+    //苹果商店版本号
+    NSString *remoteVersion = [resultDic objectForKey:@"version"];
+    NSArray *remoteArray = [remoteVersion componentsSeparatedByString:@"."];
+    //本地版本号
+    NSString *localVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSArray *localArray = [localVersion componentsSeparatedByString:@"."];
+    
+    //版本比较：要分三位依次比较。如果只是单纯的去掉版本号中"."再两者直接比较，有些情况下结果是错的，例如1.1.0(110)和1.0.10(1010)会得出后者大于前者的错误结果。
+    BOOL ifNeedUpdate = NO;//是否需要更新版本，默认NO
+    if (remoteArray.count >= 3 && localArray.count >= 3) {
+        //比较第一位
+        if ([remoteArray[0] integerValue] > [localArray[0] integerValue]) {
+            ifNeedUpdate = YES;
+            
+        //第一位相同，比较第二位
+        }else if ([remoteArray[0] integerValue] == [localArray[0] integerValue] && [remoteArray[1] integerValue] > [localArray[1] integerValue]) {
+            ifNeedUpdate = YES;
+            
+        //第一、二位相同，比较第三位
+        }else if ([remoteArray[0] integerValue] == [localArray[0] integerValue] && [remoteArray[1] integerValue] == [localArray[1] integerValue] && [remoteArray[2] integerValue] > [localArray[2] integerValue]) {
+            ifNeedUpdate = YES;
+        }
+    }
+    
+    //提示用户有新版本更新，点击确认后跳转苹果商店
+    if (ifNeedUpdate == YES) {
+        NSString *releaseNotes = resultDic[@"releaseNotes"];//更新内容
+        NSString *message = [NSString stringWithFormat:@"您有新的版本更新，更新内容：%@\n是否现在去下载?", releaseNotes];
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"更新提示" message:message preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        }]];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+            NSString *downLoadUrl = [resultDic objectForKey:@"trackViewUrl"];//应用程序下载网址
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:downLoadUrl] options:@{} completionHandler:nil];//跳转苹果商店
+        }]];
+        [self.navigationController presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+
+
 
 @end
